700f542465de24f34968c29e9dbfc46bf14f8b1f
sreeharikv13 <ssreeharikv@gmail.com>
2023-12-24 13:09:17 +0530

    sixth +++

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -6,6 +6,8 @@ use App\Category;
 use Illuminate\Http\Request;
 use App\Http\Requests\ProductSaveRequest;
 use App\Product;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
@@ -19,11 +21,21 @@ class ProductController extends Controller
     }
     public function save(ProductSaveRequest $request){
         $input = $request->validated();
+        
+        if($request->hasFile('image')){
+            $extention = $request->image->extension();
+            $filename = Str::random(6)."_".time()."_product.".$extention;
+            $request->image->storeAs('images',$filename);
+        }
+        $input['image'] = $filename;
         Product::create($input);
         return redirect()->route('admin.product.list')-with('message','Product saved successfully');
     }
     public function delete($id){
     $product = product::find(decrypt($id));
+    if(!empty($product->image)){
+        Storage::delete('images/'.$product->images);
+    }
     $product->delete();
     return redirect()->route('admin.product.list')->with('message','Product deleted successfully');
     }
--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -28,7 +28,7 @@
               </div>
               <!-- /.card-header -->
               <!-- form start -->
-              <form action="{{route('admin.product.save')}}"method="post">
+              <form action="{{route('admin.product.save')}}"method="post" enctype="multipart/form-data">
                 @csrf
                 <div class="card-body">
                   <div class="form-group">
@@ -52,7 +52,7 @@
                     <label for="exampleInputFile">Image</label>
                     <div class="input-group">
                       <div class="custom-file">
-                        <input type="file" class="custom-file-input" id="exampleInputFile">
+                        <input type="file" class="custom-file-input" name="image" id="exampleInputFile">
                         <label class="custom-file-label" for="exampleInputFile">Choose file</label>
                       </div>
                       <div class="input-group-append">
